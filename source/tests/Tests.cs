23351904107fc26f260b8293bb951ee5647e0de8
leonieb1 <me.leoniebuser.ch@zbw-online.ch>
2022-07-05 19:15:57 +0200

    Edit Test Source (Kopie von Menzi)

---
+++ source/tests/Tests.cs
@@ -0,0 +1,42 @@
+﻿namespace CarRent.Tests
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Tests
+    {
+        [Fact]
+        public void Foo()
+        {
+            var g = new MyGraph();
+            g.MyEvent += G_MyEvent;
+            g.MyEvent += G_MyEvent;
+            g.Foo();
+        }
+
+        private void G_MyEvent()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+
+    public class MyGraph
+    {
+        public event Action MyEvent;
+
+        public void Foo()
+        {
+            MyEvent();
+
+            var list = MyEvent.GetInvocationList();
+            foreach (Delegate? item in list)
+            {
+                item.DynamicInvoke();
+            }
+        }
+    }
+}
